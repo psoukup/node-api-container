b649e3928bada47f8cc72423d2260437effaa6ff
W Brian Leonard <william.leonard@oracle.com>
2016-12-22 10:55:07 -0500

    CORS support, new routes

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.use('/decoder', decoderRouter);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// CORS...
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
