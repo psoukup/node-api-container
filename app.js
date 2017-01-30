4ea0de292201f7654784d5dc1a5bffe8518dfc17
W Brian Leonard <william.leonard@oracle.com>
2017-01-30 09:41:30 -0500

    Moved messaging APIs to its own router.

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use('/decoder', decoderRouter);
 var beallsRouter = require('./routes/beallsRouter');
 app.use('/bealls', beallsRouter);
 
+var beallsRouter = require('./routes/messagingRouter');
+app.use('/messaging', beallsRouter);
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -16,60 +19,6 @@ app.use(bodyParser.json());
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-
-app.get('/messaging', function (req, res) {
-
-    var clientRequestId = req.query.clientRequestId;
-
-    var response = {
-        "platformId": 28380871,
-        "domainId": 12520225,
-        "owner": "W Brian Leonard",
-        "status": "officia dolore cupidatat reprehenderit",
-        "activationDate": "November 8, 2016",
-        "accountId": -21241144,
-        "clientRequestId": clientRequestId
-    };
-
-    res.send(response);
-});
-
-app.post('/messaging', function (req, res) {
-
-    //var clientRequestId = req.query.clientRequestId;
-
-    console.log(req.body);
-    
-    var defaultResponse = "Not Specified"
-    var platformId = defaultResponse;
-    var clientRequestId = defaultResponse;
-    var domainId = defaultResponse;
-    var accountId = defaultResponse;
-
-    if (typeof req.body.request !== 'undefined') {
-        console.log(req.body.request[0]);
-        console.log(req.body.request[0].platformId);
-        var platformId = req.body.request[0].platformId;
-        var clientRequestId = req.body.request[0].clientRequestId;
-        var domainId = req.body.request[0].data.domain;
-        var accountId = req.body.request[0].data.accountId;
-        console.log(platformId, domainId, accountId);
-    }
-
-    var response = {
-        "platformId": platformId,
-        "domainId": domainId,
-        "owner": "W Brian Leonard",
-        "status": "Active",
-        "activationDate": "November 11, 2001",
-        "accountId": accountId,
-        "clientRequestId": clientRequestId
-    }
-
-    res.setHeader('Content-Type', 'application/json');
-    res.send(response);
-
-})
 
 // For easy browser testing...
 app.get('/platforms/:platformId/domains/:domainId/accounts/:accountId', function (req, res) {
---
+++ routes/messagingRouter.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+
+router.use(function (req, res, next) {
+    //Enable CORS support...
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+app.get('/', function (req, res) {
+
+    var clientRequestId = req.query.clientRequestId;
+
+    var response = {
+        "platformId": 28380871,
+        "domainId": 12520225,
+        "owner": "W Brian Leonard",
+        "status": "officia dolore cupidatat reprehenderit",
+        "activationDate": "November 8, 2016",
+        "accountId": -21241144,
+        "clientRequestId": clientRequestId
+    };
+
+    res.send(response);
+});
+
+app.post('/', function (req, res) {
+
+    //var clientRequestId = req.query.clientRequestId;
+
+    console.log(req.body);
+    
+    var defaultResponse = "Not Specified"
+    var platformId = defaultResponse;
+    var clientRequestId = defaultResponse;
+    var domainId = defaultResponse;
+    var accountId = defaultResponse;
+
+    if (typeof req.body.request !== 'undefined') {
+        console.log(req.body.request[0]);
+        console.log(req.body.request[0].platformId);
+        var platformId = req.body.request[0].platformId;
+        var clientRequestId = req.body.request[0].clientRequestId;
+        var domainId = req.body.request[0].data.domain;
+        var accountId = req.body.request[0].data.accountId;
+        console.log(platformId, domainId, accountId);
+    }
+
+    var response = {
+        "platformId": platformId,
+        "domainId": domainId,
+        "owner": "W Brian Leonard",
+        "status": "Active",
+        "activationDate": "November 11, 2001",
+        "accountId": accountId,
+        "clientRequestId": clientRequestId
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(response);
+
+})
+
+module.exports = router;
+
