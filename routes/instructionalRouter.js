0110840cb3483fc7c3db5293dd9b9c56f4ebb968
W Brian Leonard <william.leonard@oracle.com>
2017-03-01 14:59:44 -0500

    Added Access-Control-Allow-Headers to CORS support.

--- routes/instructionalRouter.js
+++ routes/instructionalRouter.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.use(function (req, res, next) {
     //Enable CORS support...
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Access-Control-Allow-Headers, Accept");
     next();
 });
 
