72911b3ad8abd3b65cebebe63ad2beab3c44beb6
W Brian Leonard <william.leonard@oracle.com>
2017-04-03 11:30:49 -0400

    name

--- routes/instructionalRouter.js
+++ routes/instructionalRouter.js
@@ -83,7 +83,7 @@ router.get('/instructors/disciplines/:discipline', function (req, res) {
                                 "Schneider",
                                 "Voit"
                             ],
-                            "publisher": "Pearson Education",
+                            "publisher": "Monday Education",
                             "cover_image": "http://www.webassign.net/bdsvastro/BDSVastr02_cover_sm.jpg"
                         },
                         {
