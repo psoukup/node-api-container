27eaf3fa5fc72f5cca0c550c6d1a416b766cc081
W Brian Leonard <william.leonard@oracle.com>
2017-05-17 09:52:23 -0400

    Changed name

--- routes/instructionalRouter.js
+++ routes/instructionalRouter.js
@@ -83,7 +83,7 @@ router.get('/instructors/disciplines/:discipline', function (req, res) {
                                 "Schneider",
                                 "Voit"
                             ],
-                            "publisher": "Test Education",
+                            "publisher": "Gulfstream Education",
                             "cover_image": "http://www.webassign.net/bdsvastro/BDSVastr02_cover_sm.jpg"
                         },
                         {
