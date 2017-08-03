eae2ac0d157813a3a5678c570a84e63c1dfd96a1
W Brian Leonard <william.leonard@oracle.com>
2017-08-03 15:04:30 -0400

    Removed hard-coded Bearer Token

--- routes/instructionalRouter.js
+++ routes/instructionalRouter.js
@@ -83,7 +83,7 @@ router.get('/instructors/disciplines/:discipline', function (req, res) {
                                 "Schneider",
                                 "Voit"
                             ],
-                            "publisher": "Testing BEARER_TOKEN Education",
+                            "publisher": "Pearson Education",
                             "cover_image": "http://www.webassign.net/bdsvastro/BDSVastr02_cover_sm.jpg"
                         },
                         {
