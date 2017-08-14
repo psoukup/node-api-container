76b5a4c9e7002b3ee01a3d5dc6f7e06c6304e0ec
W Brian Leonard <brianleonard@yahoo.com>
2017-08-14 11:31:02 -0400

    kubectl rolling update test

--- gce-setup.sh
+++ gce-setup.sh
@@ -1,4 +1,4 @@
 gcloud container clusters create cluster-1 -m f1-micro -z us-east4-c
-kubectl create -f kube-deployment.yaml
+kubectl create --save-config -f kube-deployment.yaml
 kubectl create -f kube-service.yaml
 kubectl get service node-api-container --watch
--- routes/instructionalRouter.js
+++ routes/instructionalRouter.js
@@ -90,7 +90,7 @@ router.get('/instructors/disciplines/:discipline', function (req, res) {
                                 "Schneider",
                                 "Voit"
                             ],
-                            "publisher": "Pearson Education",
+                            "publisher": "GCE Education",
                             "cover_image": "http://www.webassign.net/bdsvastro/BDSVastr02_cover_sm.jpg"
                         },
                         {
